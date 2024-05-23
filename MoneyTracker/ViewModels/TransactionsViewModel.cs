a8eab83114011afd2cf12a294d388f8d0198ca90
ScooterMcTooter <slmillerjr95@gmail.com>
2024-05-23 00:29:45 -0400

    #8
    The most significant changes include the correction of the namespace declaration in `Constants.cs`, the reformatting of the `TransactionTypes` list in the same file, and the transition of `UserPage` and `UserViewModel` services from transient to singleton in `MauiProgram.cs`. Additionally, the `TransactionsPage.xaml` file was updated to include a more complex UI for handling transactions. In `HomeViewModel.cs` and `TransactionsViewModel.cs`, the `TransactionsViewModel` constructor now takes two parameters. Several properties were added to `TransactionsViewModel.cs`, and two new commands were introduced: `SaveTransaction` and `ClearTransaction`.
    
    1. The namespace declaration in `Constants.cs` was corrected from `namespace MoneyTracker-{` to `namespace MoneyTracker;`.
    2. The `TransactionTypes` list in `Constants.cs` was reformatted for better readability.
    3. The `UserPage` and `UserViewModel` services in `MauiProgram.cs` were changed from transient to singleton.
    4. The `TransactionsPage.xaml` file was updated to include a more complex UI for handling transactions.
    5. In `HomeViewModel.cs`, the `TransactionsViewModel` constructor now takes two parameters: `ApplicationDbContext` and `UserViewModel`. This change was also reflected in the `TransactionsViewModel.cs` file.
    6. The `TransactionDate` property in `HomeViewModel.cs` was renamed to `Date` and its value is now retrieved using `GetValueOrDefault()`.
    7. In `TransactionsViewModel.cs`, several properties were added, and the `User` property was updated to retrieve the user from the database.
    8. Two new commands were added to `TransactionsViewModel.cs`: `SaveTransaction` and `ClearTransaction`.

--- MoneyTracker/ViewModels/TransactionsViewModel.cs
+++ MoneyTracker/ViewModels/TransactionsViewModel.cs
@@ -1,23 +1,87 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MoneyTrackerMigrations;
+using MoneyTrackerMigrations.Models;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MoneyTracker.ViewModels
 {
     public partial class TransactionsViewModel : ObservableObject
     {
-        [Key]
-        public int Id { get; set; }
+        private readonly ApplicationDbContext _db;
+        private readonly UserViewModel _userViewModel;
+
+        public TransactionsViewModel(ApplicationDbContext db, UserViewModel userViewModel)
+        {
+            _db = db;
+            _userViewModel = userViewModel;
+            SelectedAccount = new AccountModel();
+            TransactionType = new TransactionTypeModel();
+            User = _db.userModels.Where(u => u.Id == _userViewModel.Id).First() ?? new UserModel();
+
+            Accounts = new ObservableCollection<AccountModel>(_db.accountModels.ToList());
+            TransactionTypes = new ObservableCollection<TransactionTypeModel>(_db.transactionTypeModels.ToList());
+            Transactions = new ObservableCollection<TransactionModel>(_db.transactionModels.Any() ? _db.transactionModels.Where(u => u.UserId == User.Id).ToList(): new List<TransactionModel>());
+        }
+
+        #region Observable Properties
         [ObservableProperty]
-        string? name;
+        string name = string.Empty;
         [ObservableProperty]
         double amount;
-        public UserViewModel? User { get; set; }
-        [Required]
-        public TransactionTypeViewModel? TransactionType { get; set; } // Connected to Constants.TransactionTypes
-        [Required]
-        public AccountViewModel account { get; set; } = new AccountViewModel();
-        public string? Location { get; set; }
-        [Required]
-        public DateTime TransactionDate { get; set; } = DateTime.Now;
+        [ObservableProperty]    
+        string? description;
+        [ObservableProperty]
+        TransactionTypeModel transactionType;
+        [ObservableProperty]
+        ObservableCollection<TransactionTypeModel> transactionTypes;
+        [ObservableProperty]
+        ObservableCollection<AccountModel> accounts;
+        [ObservableProperty]
+        AccountModel selectedAccount;
+        [ObservableProperty]
+        ObservableCollection<TransactionModel> transactions;
+        [ObservableProperty]
+        TransactionModel selectedTransaction;
+        [ObservableProperty]
+        DateTime? date = null;
+        #endregion
+
+        public UserModel User { get; set; }
+        public string TransactionList
+        {
+            get => $"{Name}\t{Date?.ToString("MM/dd/yyyy")}";
+        }
+
+        #region Commands
+        [RelayCommand]
+        async Task SaveTransaction()
+        {
+            // Save transaction to database
+            TransactionModel transaction = new TransactionModel
+            {
+                Name = Name,
+                Amount = Amount,
+                Notes = Description,
+                TransactionType = TransactionType,
+                Account = SelectedAccount,
+                Date = Date.HasValue ? Date.Value : DateTime.Now,
+                User = User
+            };
+
+            await _db.transactionModels.AddAsync(transaction);
+            await _db.SaveChangesAsync();
+        }
+
+        [RelayCommand]
+        public void ClearTransaction()
+        {
+            Name = string.Empty;
+            Amount = 0;
+            Description = string.Empty;
+            TransactionType = new TransactionTypeModel();
+        }
+        #endregion
     }
 }
